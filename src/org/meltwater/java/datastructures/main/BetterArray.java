8d23e457fb57bc349233059c8a265c89f281d889
= <roselynemakena@gmail.com>
2015-10-08 22:42:39 +0000

    Modified BST.java

--- src/org/meltwater/java/datastructures/main/BetterArray.java
+++ src/org/meltwater/java/datastructures/main/BetterArray.java
@@ -1,36 +1,70 @@
 package org.meltwater.java.datastructures.main;
 
 public class BetterArray {
-
-   
-
-        public void insert (int position, int x){
-            
-        }
-        public void shift (int position, int x){
-            
-        }
-        public void index (int position){
-            
-        }
-        public void contains (int position){
-            
-        }public void get (int position){
-            
-        }
-        public void size (int position){
-            
-        }
-         public void isEmpty (int position, E element){
-            
-        } public void remove (int position, E element){
-            
-        } public void remove(int position, E element){
-            
-        } public void reverse(int position, E element){
-            
-        }
-         public void toString(int position, E element){
-            
-        }
+    
+    BetterArray left;
+    BetterArray right;
+    int key;
+    //int k;
+    
+    public BetterArray(int key){
         
+       this.key  = key; 
+        
+        
+    }
+    
+    public void add( int x)
+    {
+        BetterArray n  = null; 
+        //if the node key is 
+        if(n.key < x){
+            
+            //go right
+            
+            
+        }
+        else if(n.key>x){
+            //go left
+            
+        }
+        else{
+            
+            
+        }
+            
+        
+    }
+    
+    public void remove()
+    {
+        
+    }
+    
+    public void size()
+    {
+        
+    }
+    
+    public void contains()
+    {
+        
+    }
+    
+    public void rcontains()
+    {
+        
+    }
+    
+    public void smallest()
+    {
+        
+    }
+    
+    public void largest()
+    {
+        
+    }
+    
+
+}
