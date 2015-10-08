93e3d07c7a7c69dbeca8705554730e71cab7aefd
Roselyne <roselynemakena@gmail.com>
2015-10-08 07:11:29 +0000

    Initial commit

---
+++ src/org/meltwater/java/datastructures/main/Main.java
@@ -0,0 +1,15 @@
+package org.meltwater.java.datastructures.main;
+
+
+public class Main {
+    
+    
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("");
+    }
+    
+}
---
+++ src/org/meltwater/java/datastructures/main/SinglyLinked.java
@@ -0,0 +1,8 @@
+package org.meltwater.java.datastructures.main;
+
+/**
+ @author makena
+ */
+public class SinglyLinked {
+
+}
